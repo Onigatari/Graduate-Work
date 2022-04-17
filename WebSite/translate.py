6e1d798573a257526f3cb411cd00cdfed5af4f09
Onigatari <gamosov.gs@yandex.ru>
2022-04-17 17:49:11 +0300

    Version 0.6.0

--- WebSite/translate.py
+++ WebSite/translate.py
@@ -1,134 +1,142 @@
-import tensorflow as tf
-import pickle as pkl
-import string
-
-import re, os
-import pandas as pd
 import numpy as np
+import pandas as pd
 
-from keras.preprocessing.text import Tokenizer
-from keras.models import model_from_json
-from keras.models import Model, load_model
-from keras.layers import LSTM, GRU, Input, Dense, Embedding
-from keras.preprocessing.sequence import pad_sequences
+import typing
+from typing import Any, Tuple
 
-start_target = "<sos>"
-end_target = "<eos>"
+import tensorflow as tf
 
-HIDDEN_DIM = 256
+import tensorflow_text as tf_text
 
-type_mode = {
-    "0" : "LSTM-[rus-eng]-[Epochs=1]-[LossFunction=categorical_crossentropy]",
-    "1" : "rus-oss.txt-180"
+class Encoder(tf.keras.layers.Layer):
+    def __init__(self, input_vocab_size, embedding_dim, enc_units):
+        super(Encoder, self).__init__()
+        self.enc_units = enc_units
+        self.input_vocab_size = input_vocab_size
+
+        self.embedding = tf.keras.layers.Embedding(self.input_vocab_size, embedding_dim)
+
+        self.gru = tf.keras.layers.GRU(self.enc_units,
+                                       return_sequences=True,
+                                       return_state=True,
+                                       recurrent_initializer='glorot_uniform')
+
+class BahdanauAttention(tf.keras.layers.Layer):
+    def __init__(self, units):
+        super().__init__()
+
+        self.W1 = tf.keras.layers.Dense(units, use_bias=False)
+        self.W2 = tf.keras.layers.Dense(units, use_bias=False)
+
+        self.attention = tf.keras.layers.AdditiveAttention()
+
+class DecoderInput(typing.NamedTuple):
+    new_tokens: Any
+    enc_output: Any
+    mask: Any
+
+class DecoderOutput(typing.NamedTuple):
+    logits: Any
+    attention_weights: Any
+
+class Decoder(tf.keras.layers.Layer):
+    def __init__(self, output_vocab_size, embedding_dim, dec_units):
+        super(Decoder, self).__init__()
+        self.dec_units = dec_units
+        self.output_vocab_size = output_vocab_size
+        self.embedding_dim = embedding_dim
+
+        self.embedding = tf.keras.layers.Embedding(self.output_vocab_size, embedding_dim)
+
+        self.gru = tf.keras.layers.GRU(self.dec_units,
+                                       return_sequences=True,
+                                       return_state=True,
+                                       recurrent_initializer='glorot_uniform')
+
+        self.attention = BahdanauAttention(self.dec_units)
+
+        self.Wc = tf.keras.layers.Dense(dec_units, activation=tf.math.tanh, use_bias=False)
+
+        self.fc = tf.keras.layers.Dense(self.output_vocab_size)
+
+
+class Translator(tf.Module):
+    def __init__(self, encoder, decoder, input_text_processor, output_text_processor):
+        self.encoder = encoder
+        self.decoder = decoder
+        self.input_text_processor = input_text_processor
+        self.output_text_processor = output_text_processor
+
+        self.output_token_string_from_index = (
+            tf.keras.layers.StringLookup(
+                vocabulary=output_text_processor.get_vocabulary(),
+                mask_token='',
+                invert=True))
+
+        index_from_string = tf.keras.layers.StringLookup(
+            vocabulary=output_text_processor.get_vocabulary(), mask_token='')
+        token_mask_ids = index_from_string(['', '[UNK]', '[START]']).numpy()
+
+        token_mask = np.zeros([index_from_string.vocabulary_size()], dtype=np.bool)
+        token_mask[np.array(token_mask_ids)] = True
+        self.token_mask = token_mask
+
+        self.start_token = index_from_string(tf.constant('[START]'))
+        self.end_token = index_from_string(tf.constant('[END]'))
+
+    def translate(self, input_text, *, max_length=50, return_attention=True, temperature=1.0):
+        batch_size = tf.shape(input_text)[0]
+        input_tokens = self.input_text_processor(input_text)
+        enc_output, enc_state = self.encoder(input_tokens)
+
+        dec_state = enc_state
+        new_tokens = tf.fill([batch_size, 1], self.start_token)
+
+        result_tokens = []
+        attention = []
+        done = tf.zeros([batch_size, 1], dtype=tf.bool)
+
+        for _ in range(max_length):
+            dec_input = DecoderInput(new_tokens=new_tokens,
+                                     enc_output=enc_output,
+                                     mask=(input_tokens != 0))
+
+            dec_result, dec_state = self.decoder(dec_input, state=dec_state)
+
+            attention.append(dec_result.attention_weights)
+
+            new_tokens = self.sample(dec_result.logits, temperature)
+
+            done = done | (new_tokens == self.end_token)
+            new_tokens = tf.where(done, tf.constant(0, dtype=tf.int64), new_tokens)
+
+            result_tokens.append(new_tokens)
+
+            if tf.executing_eagerly() and tf.reduce_all(done):
+                break
+
+        result_tokens = tf.concat(result_tokens, axis=-1)
+        result_text = self.tokens_to_text(result_tokens)
+
+        if return_attention:
+            attention_stack = tf.concat(attention, axis=1)
+            return {'text': result_text, 'attention': attention_stack}
+        else:
+            return {'text': result_text}
+
+    @tf.function(input_signature=[tf.TensorSpec(dtype=tf.string, shape=[None])])
+    def tf_translate(self, input_text):
+        return self.translate(input_text)
+
+path_model = {
+    '0': 'static/model/translator-rus-eng-mini',
+    '1': 'static/model/translator-rus-oss',
 }
 
+def translation(input_sequence, type):
+    reloaded = tf.saved_model.load(path_model[type])
+    input_text = tf.constant([input_sequence])
 
-class Encoder(tf.keras.Model):
-    """
-        Энкодер
-    """
+    result = reloaded.tf_translate(input_text)
 
-    def __init__(self, vocab_size_input, HIDDEN_DIM):
-        super(Encoder, self).__init__()
-
-        self.inputs = Input(shape=(None,), name="encoder_inputs")
-        self.embedding = Embedding(vocab_size_input, HIDDEN_DIM, mask_zero=True, name="encoder_embedding")(self.inputs)
-
-        self.encoder = LSTM(HIDDEN_DIM, return_state=True, name="encoder_lstm")
-        self.outputs, state_h, state_c = self.encoder(self.embedding)
-        self.states = [state_h, state_c]
-
-    @staticmethod
-    def getModel(model):
-        inputs_inf = model.input[0]
-        outputs_inf, inf_state_h, inf_state_c = model.layers[4].output
-        inf_states = [inf_state_h, inf_state_c]
-
-        return Model(inputs_inf, inf_states, name='Encoder')
-
-
-class Decoder(tf.keras.Model):
-    """
-        Декодер
-    """
-
-    def __init__(self, vocab_size_output, HIDDEN_DIM, encoder_states):
-        super(Decoder, self).__init__()
-
-        self.inputs = Input(shape=(None,), name="decoder_inputs")
-        self.embedding = Embedding(vocab_size_output, HIDDEN_DIM, mask_zero=True, name="decoder_embedding")(self.inputs)
-
-        self.decoder = LSTM(HIDDEN_DIM, return_sequences=True, return_state=True, name="decoder_lstm")
-        self.outputs, _, _ = self.decoder(self.embedding, initial_state=encoder_states)
-        self.dense = Dense(vocab_size_output, activation='softmax', name="dense_lstm")
-        self.outputs = self.dense(self.outputs)
-
-    @staticmethod
-    def getModel(model):
-        state_h_input = Input(shape=(HIDDEN_DIM,))
-        state_c_input = Input(shape=(HIDDEN_DIM,))
-        state_input = [state_h_input, state_c_input]
-
-        input_inf = model.input[1]
-        emb_inf = model.layers[3](input_inf)
-        lstm_inf = model.layers[5]
-        output_inf, state_h_inf, state_c_inf = lstm_inf(emb_inf, initial_state=state_input)
-        state_inf = [state_h_inf, state_c_inf]
-        dense_inf = model.layers[6]
-        output_final = dense_inf(output_inf)
-
-        return Model([input_inf] + state_input, [output_final] + state_inf, name='Decoder')
-
-def load_model_and_weights(path):
-    json_file = open(f'static/date/{path}.json')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    model_loaded = model_from_json(loaded_model_json)
-
-    model_loaded.load_weights(f'static/date/{path}-[weight].h5')
-
-    return model_loaded
-
-def load_tokenizers(path):
-    with open(f'static/date/{path}-[input].pkl', 'rb') as f:
-        tokenizer_input = pkl.load(f)
-    with open(f'static/date/{path}-[output].pkl', 'rb') as f:
-        tokenizer_output = pkl.load(f)
-
-    return tokenizer_input, tokenizer_output
-
-def predict(input_seq, encoder_model, decoder_model, tokenizer_output, reverse_word_map_target):
-    state_values_encoder = encoder_model.predict(input_seq)
-    target_seq = np.zeros((1, 1))
-    target_seq[0, 0] = tokenizer_output.word_index[start_target]
-    stop_condition = False
-    decoder_sentance = ''
-
-    while not stop_condition:
-        sample_word, decoder_h, decoder_c = decoder_model.predict([target_seq] + state_values_encoder)
-        sample_word_index = np.argmax(sample_word[0, -1, :])
-        decoder_word = reverse_word_map_target[sample_word_index]
-        decoder_sentance += ' ' + decoder_word
-        if (decoder_word == end_target or
-                len(decoder_sentance) > 70):
-            stop_condition = True
-        target_seq[0, 0] = sample_word_index
-        state_values_encoder = [decoder_h, decoder_c]
-    return decoder_sentance
-
-def translation(sentance, type):
-    path = type_mode[type]
-    model = load_model_and_weights(path)
-    tokenizer_input, tokenizer_output = load_tokenizers(path)
-
-    encoder_model = Encoder.getModel(model)
-    decoder_model = Decoder.getModel(model)
-
-    reverse_word_map_target = dict(map(reversed, tokenizer_output.word_index.items()))
-
-    input_seq = tokenizer_input.texts_to_sequences([sentance])
-    pad_sequence = pad_sequences(input_seq, maxlen=30, padding='post')
-    predicted_target = predict(pad_sequence, encoder_model, decoder_model, tokenizer_output, reverse_word_map_target)
-
-    hypothetic = str(predicted_target[:-5]).strip().capitalize()
-    return hypothetic
+    return result['text'][0].numpy().decode().capitalize()

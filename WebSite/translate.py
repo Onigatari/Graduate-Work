99636661e3661f0c0e5b1139db8bf868f0bdd393
Onigatari <gamosov.gs@yandex.ru>
2022-06-23 13:42:00 +0300

    Version 0.9

--- WebSite/translate.py
+++ WebSite/translate.py
@@ -1,142 +1,28 @@
-import numpy as np
-import pandas as pd
-
-import typing
-from typing import Any, Tuple
-
 import tensorflow as tf
-
 import tensorflow_text as tf_text
+import model
 
-class Encoder(tf.keras.layers.Layer):
-    def __init__(self, input_vocab_size, embedding_dim, enc_units):
-        super(Encoder, self).__init__()
-        self.enc_units = enc_units
-        self.input_vocab_size = input_vocab_size
-
-        self.embedding = tf.keras.layers.Embedding(self.input_vocab_size, embedding_dim)
-
-        self.gru = tf.keras.layers.GRU(self.enc_units,
-                                       return_sequences=True,
-                                       return_state=True,
-                                       recurrent_initializer='glorot_uniform')
-
-class BahdanauAttention(tf.keras.layers.Layer):
-    def __init__(self, units):
-        super().__init__()
-
-        self.W1 = tf.keras.layers.Dense(units, use_bias=False)
-        self.W2 = tf.keras.layers.Dense(units, use_bias=False)
-
-        self.attention = tf.keras.layers.AdditiveAttention()
-
-class DecoderInput(typing.NamedTuple):
-    new_tokens: Any
-    enc_output: Any
-    mask: Any
-
-class DecoderOutput(typing.NamedTuple):
-    logits: Any
-    attention_weights: Any
-
-class Decoder(tf.keras.layers.Layer):
-    def __init__(self, output_vocab_size, embedding_dim, dec_units):
-        super(Decoder, self).__init__()
-        self.dec_units = dec_units
-        self.output_vocab_size = output_vocab_size
-        self.embedding_dim = embedding_dim
-
-        self.embedding = tf.keras.layers.Embedding(self.output_vocab_size, embedding_dim)
-
-        self.gru = tf.keras.layers.GRU(self.dec_units,
-                                       return_sequences=True,
-                                       return_state=True,
-                                       recurrent_initializer='glorot_uniform')
-
-        self.attention = BahdanauAttention(self.dec_units)
-
-        self.Wc = tf.keras.layers.Dense(dec_units, activation=tf.math.tanh, use_bias=False)
-
-        self.fc = tf.keras.layers.Dense(self.output_vocab_size)
-
-
-class Translator(tf.Module):
-    def __init__(self, encoder, decoder, input_text_processor, output_text_processor):
-        self.encoder = encoder
-        self.decoder = decoder
-        self.input_text_processor = input_text_processor
-        self.output_text_processor = output_text_processor
-
-        self.output_token_string_from_index = (
-            tf.keras.layers.StringLookup(
-                vocabulary=output_text_processor.get_vocabulary(),
-                mask_token='',
-                invert=True))
-
-        index_from_string = tf.keras.layers.StringLookup(
-            vocabulary=output_text_processor.get_vocabulary(), mask_token='')
-        token_mask_ids = index_from_string(['', '[UNK]', '[START]']).numpy()
-
-        token_mask = np.zeros([index_from_string.vocabulary_size()], dtype=np.bool)
-        token_mask[np.array(token_mask_ids)] = True
-        self.token_mask = token_mask
-
-        self.start_token = index_from_string(tf.constant('[START]'))
-        self.end_token = index_from_string(tf.constant('[END]'))
-
-    def translate(self, input_text, *, max_length=50, return_attention=True, temperature=1.0):
-        batch_size = tf.shape(input_text)[0]
-        input_tokens = self.input_text_processor(input_text)
-        enc_output, enc_state = self.encoder(input_tokens)
-
-        dec_state = enc_state
-        new_tokens = tf.fill([batch_size, 1], self.start_token)
-
-        result_tokens = []
-        attention = []
-        done = tf.zeros([batch_size, 1], dtype=tf.bool)
-
-        for _ in range(max_length):
-            dec_input = DecoderInput(new_tokens=new_tokens,
-                                     enc_output=enc_output,
-                                     mask=(input_tokens != 0))
-
-            dec_result, dec_state = self.decoder(dec_input, state=dec_state)
-
-            attention.append(dec_result.attention_weights)
-
-            new_tokens = self.sample(dec_result.logits, temperature)
-
-            done = done | (new_tokens == self.end_token)
-            new_tokens = tf.where(done, tf.constant(0, dtype=tf.int64), new_tokens)
-
-            result_tokens.append(new_tokens)
-
-            if tf.executing_eagerly() and tf.reduce_all(done):
-                break
-
-        result_tokens = tf.concat(result_tokens, axis=-1)
-        result_text = self.tokens_to_text(result_tokens)
-
-        if return_attention:
-            attention_stack = tf.concat(attention, axis=1)
-            return {'text': result_text, 'attention': attention_stack}
-        else:
-            return {'text': result_text}
-
-    @tf.function(input_signature=[tf.TensorSpec(dtype=tf.string, shape=[None])])
-    def tf_translate(self, input_text):
-        return self.translate(input_text)
+cur_model = ""
+reloaded = None
 
 path_model = {
     '0': 'RUS-OSS',
     '1': 'RUS-ENG-MINI',
 }
 
-def translation(input_sequence, type):
-    reloaded = tf.saved_model.load(f'static/model/{path_model[type]}')
-    input_text = tf.constant([input_sequence])
 
+def load_model(type):
+    global cur_model
+    global reloaded
+
+    if (cur_model != type):
+        cur_model = type
+        reloaded = tf.saved_model.load(f'static/model/{path_model[type]}')
+
+def translation(input_sequence, type):
+    load_model(type)
+
+    input_text = tf.constant([input_sequence])
     result = reloaded.tf_translate(input_text)
 
     return result['text'][0].numpy().decode().capitalize()
